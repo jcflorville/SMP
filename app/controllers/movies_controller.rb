e9b2bbbb9c2e944e94a5d0cdbc6f2d879e120655
Juan C <jcflorville@gmail.com>
2012-03-23 22:10:22 -0430

    Edit Index New Movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
 	
 	def index
-		
+		@movies = Movie.limit(10)
 	end
 
 	def new
@@ -10,11 +10,24 @@ class MoviesController < ApplicationController
 
 	def create
 		@movie = Movie.new params[:movie]
+		p @movie
 		@movie.save
 		redirect_to :back
 	end
 
 	def edit
-		
+		@movie = Movie.find(params[:id])
+	end
+
+	def update
+		@movie = Movie.find(params[:id])
+		@movie.update_attributes! params[:movie]
+		redirect_to movies_path
+	end
+
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
+		redirect_to movies_path
 	end
 end
