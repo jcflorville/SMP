e15e1aa2902e45f055809ceb808d57e9d23cb61c
Juan A. Gonzalez <jags84@gmail.com>
2012-04-01 19:39:52 -0430

    Jquery Delete
    Borre various .JS, quite el menu Herramienta y puse direct a /Home

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ SMP::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root :to => 'login#index'
+    root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
